a34f048ff7e7e45603146db89644b3ac8dbfdd29
Filipe Esperandio <filipesperandio@gmail.com>
2015-05-08 16:07:04 -0300

    make better use of gloablly loaded variable exports and make deployment ssh connection extensible

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,19 @@
 #!/usr/bin/env bash
+set -e
 
 usage(){
-  echo "${BASH_SOURCE[0]} -s <server-host> -c <certificate-path> -u <user>"
+  echo "${BASH_SOURCE[0]} <ssh-connection-string>"
   exit 1
 }
 
 [[ $# -eq 0 ]] && usage
 
-while getopts s:c:u:h OPT; do
-  case $OPT in
-    s) 
-      SERVER=$OPTARG
-      ;;
-    c)
-      CERT=$OPTARG
-      ;;
-    u)
-      USER=$OPTARG
-      ;;
-    h|\?)
-      usage
-      ;;
-  esac
-done
 
 git push origin master
 
-ssh -i ${CERT} ${USER}@${SERVER} << EOF
-  set -x
+ssh $@ << EOF
   cd dockerized-jenkins
   git pull origin master
-  \$(cat ~/.bashrc | grep export)
   make rebuild
 EOF
 
