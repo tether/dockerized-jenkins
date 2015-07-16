98298d5654c3f9654073aef92aeaf1ff6bed2d75
Fabio Rehm <fgrehm@gmail.com>
2015-07-16 17:34:57 -0300

    Bump to docker-compose 1.3.3

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -11,7 +11,7 @@ install_docker() {
 }
 
 install_docker_compose() {
-  curl -L https://github.com/docker/compose/releases/download/1.3.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.3.3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
 
